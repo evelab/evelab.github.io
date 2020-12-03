c47629d1c382022ba1390649d98d08dab5859c1f
raffmares <crmares@tuta.io>
2020-12-03 22:57:08 +0000

    Shadow edits

--- scripts/mobile_menu.js
+++ scripts/mobile_menu.js
@@ -3,11 +3,11 @@ function toggleMenu() {
   var x = document.getElementById('pages');
   if (x.style.maxHeight === '300px') {
     x.style.maxHeight = '0';
-    x.style.boxShadow = '0 0 4px 2px rgb(60, 60, 60)';
+    x.style.boxShadow = '0 0 4px 2px rgba(60, 60, 60, 0.95)';
     document.getElementById('content').removeEventListener('click', closeMenu);
   } else {
     x.style.maxHeight = '300px';
-    x.style.boxShadow = '0 5px 4px -5px rgb(30, 30, 30)';
+    x.style.boxShadow = '0 5px 4px -4px rgba(20, 20, 20, 0.95)';
     document.getElementById('content').addEventListener('click', closeMenu);
   }
 }
@@ -16,6 +16,6 @@ function toggleMenu() {
 function closeMenu() {
   var x = document.getElementById('pages');
   x.style.maxHeight = '0';
-  x.style.boxShadow = '0 0 4px 2px rgb(60, 60, 60)';
+  x.style.boxShadow = '0 0 4px 2px rgba(60, 60, 60, 0.95)';
   document.getElementById('content').removeEventListener('click', closeMenu);
 }
