bcda1566ac03e195a8d5cdd08537e0eeedd15df4
raffmares <crmares@tuta.io>
2020-12-03 22:44:13 +0000

    Minor change

--- scripts/mobile_menu.js
+++ scripts/mobile_menu.js
@@ -7,7 +7,7 @@ function toggleMenu() {
     document.getElementById('content').removeEventListener('click', closeMenu);
   } else {
     x.style.maxHeight = '300px';
-    x.style.boxShadow = '0 6px 4px -4px rgb(30, 30, 30)';
+    x.style.boxShadow = '0 5px 4px -5px rgb(30, 30, 30)';
     document.getElementById('content').addEventListener('click', closeMenu);
   }
 }
