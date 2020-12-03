854f89cee5db8884b54b724a1b66a776ef9fbfe1
raffmares <crmares@tuta.io>
2020-12-03 23:03:00 +0000

    Reverted style for mobile

--- scripts/mobile_menu.js
+++ scripts/mobile_menu.js
@@ -3,11 +3,9 @@ function toggleMenu() {
   var x = document.getElementById('pages');
   if (x.style.maxHeight === '300px') {
     x.style.maxHeight = '0';
-    x.style.boxShadow = '0 0 4px 2px rgba(60, 60, 60, 0.95)';
     document.getElementById('content').removeEventListener('click', closeMenu);
   } else {
     x.style.maxHeight = '300px';
-    x.style.boxShadow = '0 5px 4px -4px rgba(20, 20, 20, 0.95)';
     document.getElementById('content').addEventListener('click', closeMenu);
   }
 }
@@ -16,6 +14,5 @@ function toggleMenu() {
 function closeMenu() {
   var x = document.getElementById('pages');
   x.style.maxHeight = '0';
-  x.style.boxShadow = '0 0 4px 2px rgba(60, 60, 60, 0.95)';
   document.getElementById('content').removeEventListener('click', closeMenu);
 }
