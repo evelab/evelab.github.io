2f252e51f9edc8f7316d9ac0c5bc79dfbd1cb641
raffmares <crmares@tuta.io>
2020-12-03 22:36:08 +0000

    Minor edit

--- scripts/mobile_menu.js
+++ scripts/mobile_menu.js
@@ -3,9 +3,11 @@ function toggleMenu() {
   var x = document.getElementById('pages');
   if (x.style.maxHeight === '300px') {
     x.style.maxHeight = '0';
+    x.style.boxShadow = '0 0 4px 2px rgb(60, 60, 60)';
     document.getElementById('content').removeEventListener('click', closeMenu);
   } else {
     x.style.maxHeight = '300px';
+    x.style.boxShadow = '0 6px 4px -4px rgb(30, 30, 30)';
     document.getElementById('content').addEventListener('click', closeMenu);
   }
 }
@@ -14,5 +16,6 @@ function toggleMenu() {
 function closeMenu() {
   var x = document.getElementById('pages');
   x.style.maxHeight = '0';
+  x.style.boxShadow = '0 0 4px 2px rgb(60, 60, 60)';
   document.getElementById('content').removeEventListener('click', closeMenu);
 }
