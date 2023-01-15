e8f6ffc43f07d707b5c6bbc3f2ca41b8740e23f6
raffmares <crmares@tuta.io>
2023-01-15 15:38:11 +0000

    Updated Jekyll and updates to website

--- Gemfile
+++ Gemfile
@@ -2,10 +2,8 @@
 
 source "https://rubygems.org"
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
-
 # gem "rails"
 
-gem "jekyll", "~> 4.0"
+gem "jekyll", "~> 4.2"
 
-gem 'wdm', '>= 0.1.0' if Gem.win_platform?
+gem "webrick", "~> 1.7"
--- scripts/news.js
+++ scripts/news.js
@@ -34,7 +34,7 @@ function populateNews(news) {
         let twitter = document.createElement('td');
         twitter.className = 'twitter';
         twitter.rowSpan = '5';
-        twitter.innerHTML = "<a class='twitter-timeline' data-height='440' data-width='100%' data-link-color='#2B7BB9' data-chrome='noheader nofooter noborders' href='https://twitter.com/englishse?ref_src=twsrc%5Etfw'></a>";
+        twitter.innerHTML = "<a class='twitter-timeline' data-height='440' data-width='100%'  data-chrome='nofooter transparent' data-tweet-limit='10' href='https://twitter.com/englishse?ref_src=twsrc%5Etfw'>Tweets from @englishse</a>";
         tr.appendChild(twitter);
       }
       document.getElementById('newsLatest').appendChild(tr);
