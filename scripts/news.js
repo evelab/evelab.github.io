a8e1071c6e1c8c3b3aa41c2c81f554013945147f
raffmares <crmares@tuta.io>
2023-10-28 21:17:24 +0100

    Updates

--- scripts/news.js
+++ scripts/news.js
@@ -9,14 +9,14 @@ requestNews.onload = function() {
 }
 
 // get events from events.json, and create and populate events rows
-let requestEvents = new XMLHttpRequest();
-requestEvents.open('GET', 'news-events/events.json');
-requestEvents.responseType = 'json';
-requestEvents.send();
-requestEvents.onload = function() {
-  const events = requestEvents.response;
-  populateEvents(events);
-}
+// let requestEvents = new XMLHttpRequest();
+// requestEvents.open('GET', 'news-events/events.json');
+// requestEvents.responseType = 'json';
+// requestEvents.send();
+// requestEvents.onload = function() {
+//   const events = requestEvents.response;
+//   populateEvents(events);
+// }
 
 function populateNews(news) {
   for (i = 0; i < news.length; i++) {
@@ -31,11 +31,11 @@ function populateNews(news) {
     if(i < 5) {
       if(i == 0) {
         date.className = 'date';
-        let twitter = document.createElement('td');
-        twitter.className = 'twitter';
-        twitter.rowSpan = '5';
-        twitter.innerHTML = "<a class='twitter-timeline' data-height='440' data-width='100%'  data-chrome='nofooter transparent' data-tweet-limit='10' href='https://twitter.com/englishse?ref_src=twsrc%5Etfw'>Tweets from @englishse</a>";
-        tr.appendChild(twitter);
+      //   let twitter = document.createElement('td');
+      //   twitter.className = 'twitter';
+      //   twitter.rowSpan = '5';
+      //   twitter.innerHTML = "<a class='twitter-timeline' data-height='440' data-width='100%'  data-chrome='nofooter transparent' data-tweet-limit='10' href='https://twitter.com/englishse?ref_src=twsrc%5Etfw'>Tweets from @englishse</a>";
+      //   tr.appendChild(twitter);
       }
       document.getElementById('newsLatest').appendChild(tr);
     }
@@ -49,18 +49,18 @@ function populateNews(news) {
   }
 }
 
-function populateEvents(events) {
-  for (i = 0; i < events.length; i++) {
-    let tr = document.createElement('tr');
-    let date = document.createElement('td');
-    let text = document.createElement('td');
-    date.textContent = events[i].date;
-    text.innerHTML = events[i].text;
-    tr.appendChild(date);
-    tr.appendChild(text);
-    if(i == 0) {
-      date.className = 'date';
-    }
-    document.getElementById('events').appendChild(tr);
-  }
-}
+// function populateEvents(events) {
+//   for (i = 0; i < events.length; i++) {
+//     let tr = document.createElement('tr');
+//     let date = document.createElement('td');
+//     let text = document.createElement('td');
+//     date.textContent = events[i].date;
+//     text.innerHTML = events[i].text;
+//     tr.appendChild(date);
+//     tr.appendChild(text);
+//     if(i == 0) {
+//       date.className = 'date';
+//     }
+//     document.getElementById('events').appendChild(tr);
+//   }
+// }
